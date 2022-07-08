31cb2d317edc19c7d1e22a75cfe949d2c0cb51ed
slava-vl <slavabolotaev@gmail.com>
2022-07-08 11:51:48 +0300

    added search

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:iron_dictionary/dummy_data.dart';
 
+import '../models/word.dart';
 import '../widgets/word_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Word> _words;
+  bool _isSearchingMode = false;
+
+  @override
+  void initState() {
+    _words = words;
+    super.initState();
+  }
+
+  void _toggleSearchingMode() {
+    setState(() {
+      _isSearchingMode = !_isSearchingMode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,27 +37,74 @@ class HomePage extends StatelessWidget {
           'ID',
           style: TextStyle(color: Colors.black, fontSize: 30),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _toggleSearchingMode();
+            },
+            icon: Icon(
+              Icons.search_outlined,
+              color: Colors.black,
+            ),
+          )
+        ],
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TextField(),
+            AnimatedCrossFade(
+              firstChild: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      value = value.toLowerCase().trim();
+                      _words = words
+                          .where((element) =>
+                              element.origin.toLowerCase().contains(value)
+                              || element.translate.toLowerCase().contains(value)
+                              )
+                              
+                          .toList();
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Поиск',
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.close),
+                      onPressed: _toggleSearchingMode,
+                    ),
+                  ),
+                ),
+              ),
+              secondChild: SizedBox(width: double.infinity),
+              duration: Duration(milliseconds: 200),
+              crossFadeState: _isSearchingMode
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+            ),
             Column(
               children: [
-                ...words.map((word) => WordCard(
-                      origin: word.origin,
-                      translate: word.translate,
-                    )).toList(),
-                    ...words.map((word) => WordCard(
-                      origin: word.origin,
-                      translate: word.translate,
-                    )).toList(),
-                    ...words.map((word) => WordCard(
-                      origin: word.origin,
-                      translate: word.translate,
-                    )).toList(),
-                    
+                ..._words
+                    .map((word) => WordCard(
+                          origin: word.origin,
+                          translate: word.translate,
+                        ))
+                    .toList(),
+                ..._words
+                    .map((word) => WordCard(
+                          origin: word.origin,
+                          translate: word.translate,
+                        ))
+                    .toList(),
+                ..._words
+                    .map((word) => WordCard(
+                          origin: word.origin,
+                          translate: word.translate,
+                        ))
+                    .toList(),
               ],
             )
           ],
--- lib/widgets/word_card.dart
+++ lib/widgets/word_card.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class WordCard extends StatelessWidget {
@@ -6,25 +5,35 @@ class WordCard extends StatelessWidget {
   final String translate;
 
   const WordCard({
-    Key? key,required this.origin,required this.translate,
+    Key? key,
+    required this.origin,
+    required this.translate,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-                color: Colors.black,
-                offset: Offset(0, 1),
-                blurRadius: 5)
-          ]),
-      child: Text('$origin/$translate'),
-    );
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(color: Colors.grey, offset: Offset(0, 1), blurRadius: 5)
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              origin.toUpperCase()[0] + origin.toLowerCase().substring(1),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Text('Перевод: ${translate.toLowerCase()}')
+          ],
+        ));
   }
 }
